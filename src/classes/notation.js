c42b5a89ff86fcd285f839e269a367dd3dd02d60
Jawad <jawad.php@gmail.com>
2020-12-20 01:47:26 -0500

    Simplified index.js

--- src/classes/notation.js
+++ src/classes/notation.js
@@ -1,3 +1,54 @@
+/*
+// Class to generate random notations
+// import Notation from './classes/notation';
+const notation = new Notation (board);
+
+let i = 0;
+const totalGuesses = 20, speed = 1000;
+
+const notationEl = document.createElement ('span');
+notationEl.classList.add ('notations');
+boardEl.appendChild(notationEl);
+
+let prevNotation;
+const timer = setInterval ( () => {
+  
+  const randNotation = notation.guess ();
+  notationEl.textContent = randNotation.toUpperCase();
+  
+  // if (i > totalGuesses)
+  //   clearInterval (timer);
+      
+  prevNotation = randNotation;
+  i++;
+}, speed);
+
+
+let myGuess;
+// Add event listener to the board rather than each div to save on memory
+boardEl.addEventListener ('click', square => {
+  
+  myGuess = square.target.id.split('-')[1];
+  // square.target.classList.add ('black-rook');
+  if (myGuess == prevNotation) {    
+    console.log ("You guessed right!");
+  } else {
+    console.log (`Wrong guess ${myGuess}`);
+  }  
+});
+
+// document.addEventListener("DOMContentLoaded", function(e) {// do stuff })
+*/
+// Add an event listener to each of the Chess pieces
+// const piecesEl = [...boardEl.getElementsByClassName ('piece')];
+// piecesEl.forEach (piece => {
+
+//   piece.addEventListener ('click', e => {
+//     const thisPiece = e.target.parentElement.id;
+//     console.log (thisPiece);
+//   });  
+// });
+
 class Notation {
   constructor (boardEl) {
     if (boardEl===undefined)
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const pieces = new Pieces (boardEl, playAs, draggable);
 board.draw ();
 pieces.arrange ();
 
+// Check for legal moves
 new Moves (boardEl, pieces.playAs);
 
 // Programmatically flip the board
@@ -24,55 +25,4 @@ new Moves (boardEl, pieces.playAs);
 // pieces.arrange ({
 //   whiteArrangement: ['na1','bc1'],
 //   blackArrangement: ['rh8', 'bc3']
-// });
-
-/*
-// Class to generate random notations
-// import Notation from './classes/notation';
-const notation = new Notation (board);
-
-let i = 0;
-const totalGuesses = 20, speed = 1000;
-
-const notationEl = document.createElement ('span');
-notationEl.classList.add ('notations');
-boardEl.appendChild(notationEl);
-
-let prevNotation;
-const timer = setInterval ( () => {
-  
-  const randNotation = notation.guess ();
-  notationEl.textContent = randNotation.toUpperCase();
-  
-  // if (i > totalGuesses)
-  //   clearInterval (timer);
-      
-  prevNotation = randNotation;
-  i++;
-}, speed);
-
-
-let myGuess;
-// Add event listener to the board rather than each div to save on memory
-boardEl.addEventListener ('click', square => {
-  
-  myGuess = square.target.id.split('-')[1];
-  // square.target.classList.add ('black-rook');
-  if (myGuess == prevNotation) {    
-    console.log ("You guessed right!");
-  } else {
-    console.log (`Wrong guess ${myGuess}`);
-  }  
-});
-
-// document.addEventListener("DOMContentLoaded", function(e) {// do stuff })
-*/
-// Add an event listener to each of the Chess pieces
-// const piecesEl = [...boardEl.getElementsByClassName ('piece')];
-// piecesEl.forEach (piece => {
-
-//   piece.addEventListener ('click', e => {
-//     const thisPiece = e.target.parentElement.id;
-//     console.log (thisPiece);
-//   });  
 // });
